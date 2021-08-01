12883e6fd18e0ef4981c18295c15eb444928489a
UUUNG <jeawoong123@naver.com>
2021-08-02 00:05:51 +0900

    refact: server의 ranking을 routes, models, controllers 구조로 분리

---
+++ server/controllers/ranking.js
@@ -0,0 +1,146 @@
+const RANKING = require("../models/ranking")
+
+exports.read = async (req, res, next) => {
+  try {
+    const result = await RANKING.read();
+    res.json({ 
+      code: 200, 
+      result: "success", 
+      data : result 
+    });
+  }
+  catch(e) {
+    res.json({ 
+      code: 500, 
+      result: "error", 
+      message: e.message 
+    });
+  }
+}
+
+exports.readWeekly = async (req, res, next) => {
+
+  try {
+    const result = await RANKING.readWeekly();
+    
+    let poems = result;
+    let idx = 0;
+
+    for(const poem of poems){
+      const sqlReply = `
+        SELECT * 
+        FROM REPLY
+        WHERE REPLY.poemId = ?
+      `
+      const resultReply = await pool.query(sqlReply, [
+        poem.poemId
+      ])
+      
+      poems[idx]["replyList"] = resultReply[0]
+
+      idx += 1;
+    }
+    res.json({ 
+      code: 200, 
+      result: "success",  
+      data : poems
+    });
+  }
+  catch(e) {
+    res.json({ 
+      code: 500, 
+      result: "error", 
+      message: e.message 
+    });
+  }
+}
+exports.readMonthly = async (req, res, next) => {
+
+  try {
+    const result = await RANKING.readMonthly();
+    
+    let poems = result;
+    let idx = 0;
+
+    for(const poem of poems){
+      const sqlReply = `
+        SELECT * 
+        FROM REPLY
+        WHERE REPLY.poemId = ?
+      `
+      const resultReply = await pool.query(sqlReply, [
+        poem.poemId
+      ])
+      
+      poems[idx]["replyList"] = resultReply[0]
+
+      idx += 1;
+    }
+    res.json({ 
+      code: 200, 
+      result: "success",  
+      data : poems
+    });
+  }
+  catch(e) {
+    res.json({ 
+      code: 500, 
+      result: "error", 
+      message: e.message 
+    });
+  }
+}
+exports.readYearly = async (req, res, next) => {
+
+  try {
+    const result = await RANKING.readYearly();
+    
+    let poems = result;
+    let idx = 0;
+
+    for(const poem of poems){
+      const sqlReply = `
+        SELECT * 
+        FROM REPLY
+        WHERE REPLY.poemId = ?
+      `
+      const resultReply = await pool.query(sqlReply, [
+        poem.poemId
+      ])
+      
+      poems[idx]["replyList"] = resultReply[0]
+
+      idx += 1;
+    }
+    res.json({ 
+      code: 200, 
+      result: "success",  
+      data : poems
+    });
+  }
+  catch(e) {
+    res.json({ 
+      code: 500, 
+      result: "error", 
+      message: e.message 
+    });
+  }
+}
+exports.readHof = async (req, res, next) => {
+  try {
+    const result = await RANKING.readHof();
+
+    res.json({ 
+      code: 200, 
+      result: "success",  
+      data : result
+    });
+  }
+  catch(e) {
+    res.json({ 
+      code: 500, 
+      result: "error", 
+      message: e.message 
+    });
+  }
+}
